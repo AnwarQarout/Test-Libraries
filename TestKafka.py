19f5897b7f34c66989c5983e387fab0121f46a3d
Anwar <=>
2021-08-08 12:22:42 +0300

    m

--- TestKafka.py
+++ TestKafka.py
@@ -1,5 +1,6 @@
 import kafka
 import json
+import kafka.errors as KafkaError
 
 
 def json_serializer(data):
@@ -10,57 +11,89 @@ class TestKafka:
 
     def __init__(self):
         print("creating kafka...")
-        self.admin = kafka.admin.KafkaAdminClient(bootstrap_servers="localhost:9092"
+        try:
+            self.admin = kafka.admin.KafkaAdminClient(bootstrap_servers="localhost:9092"
                                                   , client_id="www"
                                                   )
-        self.consumer = kafka.consumer.KafkaConsumer(bootstrap_servers="localhost:9092")
-        self.producer = kafka.producer.KafkaProducer(bootstrap_servers="localhost:9092")
-        print("done")
+        except KafkaError.BrokerNotAvailableError:
+            print("Error! No brokers available. Please make sure the connection is"
+                  "correctly established.")
+            raise
+
+
 
     def create_topic(self, topic):
-        topics = []
-        topics.append(kafka.admin.NewTopic(name=topic, num_partitions=1, replication_factor=1))
-        self.admin.create_topics(new_topics=topic)
+        if self.check_if_topic_exists(topic) is True:
+            print("Error! A topic with this name already exists.")
+            raise KafkaError.TopicAlreadyExistsError("Error! The topic already exists.")
+        else:
+            topics = []
+            topics.append(kafka.admin.NewTopic(name=topic, num_partitions=1, replication_factor=1))
+            try:
+                self.admin.create_topics(new_topics=topics)
+            except KafkaError.BrokerNotAvailableError:
+                print("Error! No brokers available. Please make sure the connection is"
+                  "correctly established.")
+                raise
+
+    def check_if_topic_exists(self,topic):
+        topics = self.admin.list_topics()
+        for item in topics:
+            if item == topic:
+                return True
+        return False
+
 
     def delete_topic(self, topic):
-        topiccs = []
-        topiccs.append(kafka.admin.NewTopic(name=topic, num_partitions=1, replication_factor=1))
-        self.admin.delete_topics(topics=topic)
+        if self.check_if_topic_exists(topic) is False:
+            print("Error! The topic you're trying to delete does not exist.")
+            raise KafkaError.InvalidTopicError("Topic does not exist.")
+        else:
+            topics = []
+            topics.append(topic)
+            try:
+                self.admin.delete_topics(topics=topics, timeout_ms=2000)
+            except KafkaError.BrokerNotAvailableError:
+                print("Error! No brokers available. Please make sure the connection is"
+                  "correctly established.")
+                raise
 
-    def list_all_topics(self, host):
-        topics = host.admin.list_topics()
+    def list_all_topics(self):
+        topics = self.admin.list_topics()
         return topics
 
-    def send_file_to_kafka_topic(self,topic,filename):
-        file = open(filename,"r")
-        str = file.read()
-        str=json_serializer(str)
-        print(str)
-        self.producer.send(topic, value=b'hiiiiiiiiiiiiii')
+    def send_file_to_kafka_topic(self, topic, filename):
+        producer = kafka.producer.KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda x:
+        json.dumps(x).encode('utf-8'))
 
-    def kafka_consumer(self,topic,filename):
+
         file = open(filename, "r")
         str = file.read()
-        str = json_serializer(str)
-        topiccs = []
-        topiccs.append(topic)
-        print(self.consumer.bootstrap_connected())
-        print(self.consumer.topics())
-        output = self.consumer.poll()
-        print(output)
+
+        producer.send(topic=topic, value=str)
+        producer.close()
+
+        file.close()
+
+    def kafka_consumer(self, topic, filename):
+        consumer = kafka.consumer.KafkaConsumer(topic, bootstrap_servers=['localhost:9092'],
+                                                auto_offset_reset='earliest',
+                                                enable_auto_commit=True, consumer_timeout_ms=5000,
+                                                value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        file = open(filename, "w+")
+        output = ""
+
+        for message in consumer:
+            output += message.value
+
+        file.write(output)
+
+        consumer.unsubscribe()
+        consumer.close()
+
+        file.close()
+
+        return output
 
 
 
-    # def delete_topic(self,topic):
-
-    # def kafka_consumer(self,host,topic,filename):
-
-
-k = TestKafka()
-# k.create_topic("hahha")
-#print(k.list_all_topics(k))
-#k.delete_topic(["TestTodasdpic"])
-#print(k.list_all_topics(k))
-#print(k.list_all_topics(k))
-k.send_file_to_kafka_topic("rofl","file.txt")
-#k.kafka_consumer("TestTopic","file.txt")
